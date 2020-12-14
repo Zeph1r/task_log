86425d42447808d06069dcaec68a63c7f909ed4b
Vladimir Keyl <Keylvolodya@gmail.com>
2020-12-14 15:10:47 +0300

    Patch_1

--- task.py
+++ task.py
@@ -1,35 +1,53 @@
 import sqlite3
 import os.path
 
+def parcing(name_file, name_of_newfile='Log_nev.txt'):
+    '''Parcing file
+    
+    This function parcing a file of all sting and
+    find "PingWebSocketCM()" on logs. If there string 
+    is found, He push date and echo on database
+    '''
+    f = open(name_of_newfile, 'a')
+    for line in name_file: 
+        if 'PingWebSocketCM()' in line:
+            result = line.split(" ")
+            date = str(result[0] + ' ' + result[1])
+            print(date)
+            echo = str(result[5:])
+            print(echo)
+            cursor.execute("INSERT INTO logs VALUES (?, ?)", (echo, date))
+        else:
+            f.write(line)
+    f.close()
+
+
+
+
+# Check ald file of database
 check_db = os.path.exists('mydatabase.db')
-if check_db == True:
+if check_db:
     print('Существует')
     conn = sqlite3.connect("mydatabase.db")
     cursor = conn.cursor()
 else:
-    conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
+    conn = sqlite3.connect("mydatabase.db")
     cursor = conn.cursor()
     cursor.execute("""CREATE TABLE logs
                     (echo text, data text)""")
 
  
-# Сохраняем изменения
+# Saved changes
 conn.commit()
 
 
-# 1. Открыть файл
+# 1. Open file
 f = open('Log.txt', 'r+')
-# 2. Читать строчку, превращать её в множество и искать ключевое слово
-# 3. Если слово совпадает то внести в бд дату и событие лога, удалить строку
-for line in f:
-    result = line.split(" ")
-    if 'PingWebSocketCM()' in result:
-        date = str(result[0] + ' ' + result[1])
-        print(date)
-        echo = str(result[5:])
-        print(echo)
-        cursor.execute("INSERT INTO logs VALUES (?, ?)", (echo, date))
-        conn.commit()
+# 2. reading log.txt and parcing 
+parcing(f)
+# 3. If the word matches, then it is entered into the database. Else string copied on new file 
 
+        
+conn.commit()
 f.close()
 
